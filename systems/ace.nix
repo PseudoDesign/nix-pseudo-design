21c1a5e2207fbe7d03f81a215b77684b709a1e60
Adam Schafer <ams-tech@users.noreply.github.com>
2026-03-15 00:12:56 -0400

    create rpi5-configuration

--- systems/ace.nix
+++ systems/ace.nix
@@ -41,24 +41,6 @@ inputs.nixos-raspberrypi.lib.nixosSystem {
       };
       services.pcscd.enable = true;
     })
-    ({ ... }: {
-      fileSystems = {
-        "/boot/firmware" = {
-          device = "/dev/disk/by-uuid/FIRMWARE";
-          fsType = "vfat";
-          options = [
-            "noatime"
-            "noauto"
-            "x-systemd.automount"
-            "x-systemd.idle-timeout=1min"
-          ];
-        };
-        "/" = {
-          device = "/dev/disk/by-uuid/NIX_OS";
-          fsType = "ext4";
-          options = [ "noatime" ];
-        };
-      };
-    })
+    ./rpi5-configuration.nix
   ];
 }
---
+++ systems/rpi5-configuration.nix
@@ -0,0 +1,19 @@
+{ ... }: {
+  fileSystems = {
+    "/boot/firmware" = {
+      device = "/dev/disk/by-uuid/FIRMWARE";
+      fsType = "vfat";
+      options = [
+        "noatime"
+        "noauto"
+        "x-systemd.automount"
+        "x-systemd.idle-timeout=1min"
+      ];
+    };
+    "/" = {
+      device = "/dev/disk/by-uuid/NIX_OS";
+      fsType = "ext4";
+      options = [ "noatime" ];
+    };
+  };
+}
