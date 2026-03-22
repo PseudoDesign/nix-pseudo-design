aef57a0c8d9477b1dba7fbbf7a93921b601b834f
ams-tech <ams-tech@users.noreply.github.com>
2026-03-22 09:07:31 +0000

    add disko config

--- hosts/ace/hardware-configuration.nix
+++ hosts/ace/hardware-configuration.nix
@@ -21,5 +21,49 @@
       options = [ "noatime" ];
     };
   };
+  disko.devices = {
+    disk = {
+      nvme0n1 = {
+        type = "disk";
+        device = "/dev/nvme0n1";
+        content = {
+          type = "gpt";
+          partitions = {
+            luks = {
+              size = "100M";
+              content = {
+                type = "luks";
+                name = "crypted";
+                extraOpenArgs = [ ];
+                settings = {
+                  keyFile = "/home/adam/test-luks-key.txt";
+                  allowDiscards = true;
+                };
+                content = {
+                  type = "lvm_pv";
+                  vg = "pool";
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+    lvm_vg = {
+      pool = {
+        type = "lvm_vg";
+        lvs = {
+          data = {
+            size = "10M";
+            content = {
+              type = "filesystem";
+              format = "ext4";
+              mountpoint = "/data";
+            };
+          };
+        };
+      };
+    };
+  };
   boot.loader.raspberry-pi.bootloader = "kernel";
 }
