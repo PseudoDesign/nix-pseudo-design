f76298d1e0acfdc04c1740cdf1b646d8d5a987b6
ams-tech <ams-tech@users.noreply.github.com>
2026-03-22 07:07:52 +0000

    create file hardware configurations

--- flake.nix
+++ flake.nix
@@ -25,19 +25,21 @@
     }
   ) //
   {
-      nixosConfigurations = {
-        # "'ace' is a Raspberry Pi 5 in Adam's house."
-        ace = nixos-raspberrypi.lib.nixosSystemFull {
-          specialArgs = inputs;
-          modules = [
-            ({ config, pkgs, lib, nixos-raspberrypi, ... }: {
-              imports = with inputs.nixos-raspberrypi.nixosModules; [
-                  ./hosts/ace
-              ];
-            })
-          ];
-        };
+    nixosConfigurations = {
+      # "'ace' is a Raspberry Pi 5 in Adam's house."
+      ace = nixos-raspberrypi.lib.nixosSystemFull {
+        specialArgs = inputs;
+        modules = [
+          # Since we have "specialArgs" set to "inputs", we don't need to
+          # type in the parameter names again.
+          ({ ... }: {
+            imports = with inputs.nixos-raspberrypi.nixosModules; [
+                ./hosts/ace
+            ];
+          })
+        ];
       };
+    };
   };
   
 }
--- hosts/ace/default.nix
+++ hosts/ace/default.nix
@@ -1,17 +1,13 @@
 {
   home-manager,
-  nixos-raspberrypi,
   ...
 }:
 {
   imports = [
     ../../modules/users/adam.nix
+    ./hardware-configuration.nix
     home-manager.nixosModules.default
     ({ pkgs, ...}: {
-      imports = with nixos-raspberrypi.nixosModules; [
-        raspberry-pi-5.base
-        raspberry-pi-5.bluetooth
-      ];
       boot.loader.raspberry-pi.bootloader = "kernel";
       # This is the initial version of nixOS that was installed on this system.
       system.stateVersion = "25.11";
@@ -37,26 +33,6 @@
       home-manager.users.adam = import ../../home/adam.nix;
       # Needed for vscode
       programs.nix-ld.enable = true;
-    })
-
-    ({ ... }: {
-      fileSystems = {
-        "/boot/firmware" = {
-          device = "/dev/disk/by-uuid/2175-794E";
-          fsType = "vfat";
-          options = [
-            "noatime"
-            "noauto"
-            "x-systemd.automount"
-            "x-systemd.idle-timeout=1min"
-          ];
-        };
-        "/" = {
-          device = "/dev/disk/by-uuid/44444444-4444-4444-8888-888888888888";
-          fsType = "ext4";
-          options = [ "noatime" ];
-        };
-      };
     })
   ];
 }
