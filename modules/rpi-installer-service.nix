193ff9d7816e0901f70ac0082b71b665c41f3a1b
Adam Schafer <ams-tech@users.noreply.github.com>
2026-03-29 21:01:46 -0400

    automatically update installer on boot

--- modules/rpi-installer-service.nix
+++ modules/rpi-installer-service.nix
@@ -1,25 +1,41 @@
 { config, pkgs, ... }:
 
 let
+  imageUpgrade = pkgs.writeShellScriptBin "pd-nix-installer-upgrade" ''
+    clear
+    FLAKE="github:pseudodesign/nix-pseudo-design/latest#rpi5-installer"
+    nixos-rebuild switch --flake "''${FLAKE}" --refresh
+  '';
+
   installScript = pkgs.writeShellScriptBin "pd-nix-install" ''
     clear
+    FLAKE="github:pseudodesign/nix-pseudo-design/latest#ace"
+    DISK="/dev/nvme0n1"
+    while true; do
+        read -p "Install ''${FLAKE} to ''${DISK} [y/n] " yn
+        case $yn in
+            [Yy]* ) break;;
+            [Nn]* ) exit;;
+            * ) echo "Please answer yes or no.";;
+        esac
+    done
     nix run 'github:nix-community/disko/latest#disko-install' -- \
-      --flake "github:pseudodesign/nix-pseudo-design/latest#ace" \
+      --flake "''${FLAKE}" \
       --mode format \
-      --disk main /dev/nvme0n1
+      --disk main "''${DISK}"
   '';
 in
 {
-  environment.systemPackages = [ installScript ];
+  environment.systemPackages = [ installScript imageUpgrade ];
 
   services.getty.autologinUser = "root";
 
-  systemd.services.auoto-installer = {
-    description = "Auto-Installer Application";
+  systemd.services.auto-upgrade = {
+    description = "Auto-Upgrade Service";
     wantedBy = [ "multi-user.target" ];
     after = [ "systemd-user-sessions.service" "getty@tty1.service" ];
     serviceConfig = {
-      User = "kiosk";
+      User = "root";
       TTYPath = "/dev/tty1";
       StandardInput = "tty";
       StandardOutput = "tty";
@@ -28,7 +44,7 @@ in
       TTYVHangup = true;
       TTYVTDisallocate = true;
       Restart = "always";
-      ExecStart = "${installScript}/bin/pd-nix-install";
+      ExecStart = "${imageUpgrade}/bin/pd-nix-installer-upgrade";
     };
   };
 }
