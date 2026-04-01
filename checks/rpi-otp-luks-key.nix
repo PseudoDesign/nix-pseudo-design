774af55ef55a4e89763df33db9d9959f39f10c9b
Adam Schafer <ams-tech@users.noreply.github.com>
2026-03-31 20:08:51 -0400

    set overlays

--- checks/rpi-otp-luks-key.nix
+++ checks/rpi-otp-luks-key.nix
@@ -1,19 +1,20 @@
 { lib, testers }:
 let
   # Mock overlays for our system
-  mocks = final: prev: {
-        rpi-otp-private-key = final.callPackage ./packages/rpi-otp-private-key.nix { };
+  mocksOverlay = final: prev: {
+    rpi-otp-private-key = final.callPackage ./packages/rpi-otp-private-key.nix { };
   };
 in
-  testers.runNixOSTest {
-    name = "Test for rpi-otp-luks-key initrd service";
-    nodes.rpi =
-      { ... }:
-      {
-        imports = [ ../modules/rpi-otp-luks-key ];
-      };
+testers.runNixOSTest {
+  name = "Test for rpi-otp-luks-key initrd service";
+  nodes.rpi =
+    { ... }:
+    {
+      nixpkgs.overlays = [ mocksOverlay ];
+      imports = [ ../modules/rpi-otp-luks-key ];
+    };
 
-    testScript = ''
-      start_all()
-    '';
-  }
+  testScript = ''
+    start_all()
+  '';
+}
