7871fa83a87d570e133cd5205ddc83604e79b3ad
Adam Schafer <ams-tech@users.noreply.github.com>
2026-04-04 02:03:01 -0400

    passing package as option

--- checks/rpi-otp-luks-key.nix
+++ checks/rpi-otp-luks-key.nix
@@ -1,24 +1,24 @@
 { lib, testers, pkgs }:
 let
-  mockLuksGetKeyBin = pkgs.writeShellScriptBin "mock-rpi-otp-luks-key" ''
+  mockLuksKeyPackage = pkgs.writeShellScriptBin "rpi-otp-luks-key" ''
     echo "12345"
   '';
 in
 testers.runNixOSTest {
-  name = "rpi-otp-luks-key service writes output of luksGetKeyBin to luksKeyFile";
+  name = "rpi-otp-luks-key service writes output of configured helper to luksKeyFile";
   nodes.rpi =
     { ... }:
     {
       imports = [ ../modules/rpi-otp-luks-key ];
       services.rpiOtpLuksKey.enable = true;
-      services.rpiOtpLuksKey.luksKeyGetBin = "${mockLuksGetKeyBin}/bin/mock-rpi-otp-luks-key";
-      services.rpiOtpLuksKey.luksKeyFile = "/run/mock/secrets/luks.key";
+      services.rpiOtpLuksKey.package = mockLuksKeyPackage;
+      services.rpiOtpLuksKey.luksKeyFile = "/run/mock/package/luks.key";
     };
 
   testScript = ''
     start_all()
     rpi.wait_for_unit("default.target")
-    rpi.succeed('[ -f "/run/mock/secrets/luks.key" ]')
-    rpi.succeed('[ "$(tr -d "\\n" < /run/mock/secrets/luks.key)" = "12345" ]')
+    rpi.succeed('[ -f "/run/mock/package/luks.key" ]')
+    rpi.succeed('[ "$(tr -d "\\n" < /run/mock/package/luks.key)" = "12345" ]')
   '';
 }
--- modules/rpi-otp-luks-key/default.nix
+++ modules/rpi-otp-luks-key/default.nix
@@ -1,20 +1,22 @@
-{ pkgs, config, lib,  ... }: 
+{ pkgs, config, lib, ... }:
 let
   cfg = config.services.rpiOtpLuksKey;
+  luksKeyGetExe = lib.getExe cfg.package;
 in
 {
 
   options.services.rpiOtpLuksKey = {
     enable = lib.mkEnableOption "Raspberry Pi OTP LUKS Key Service";
-    luksKeyGetBin = lib.mkOption {
-      type = lib.types.path;
-      default = "${pkgs.rpi-otp-private-key}/bin/rpi-otp-private-key";
-      description = "Path to the binary used to populate the LUKS key from device-unique storage";
+    package = lib.mkOption {
+      type = lib.types.package;
+      default = pkgs.rpi-otp-luks-key;
+      defaultText = lib.literalExpression "pkgs.rpi-otp-luks-key";
+      description = "Package providing the helper that derives the LUKS key in initrd.";
     };
     luksKeySalt = lib.mkOption {
       type = lib.types.str;
       default = "default-salt";
-      description = "The salt value passed to luksKeyGetBin. Changing this after initial install will cause the disk decryption to fail.";
+      description = "The salt value passed to the helper. Changing this after initial install will cause the disk decryption to fail.";
     };
     luksKeyFile = lib.mkOption {
       type = lib.types.path;
@@ -28,21 +30,21 @@ in
     # necessitating the need for boot.initrd.systemd
     boot.initrd.systemd.enable = true;
     # Explicitly copy the helper into the initrd so the stage-1 service can execute it.
-    boot.initrd.systemd.storePaths = [ cfg.luksKeyGetBin ];
+    boot.initrd.systemd.storePaths = [ luksKeyGetExe ];
     boot.initrd.systemd.services.rpi-otp-luks-key-initrd = {
       wantedBy = [ "initrd.target" ];
       before = [
         "cryptsetup.target"
       ];
       unitConfig.DefaultDependencies = false;
-          description = "Get the luks key from Raspberry Pi OTP.";
+      description = "Get the luks key from Raspberry Pi OTP.";
       serviceConfig = {
         Type = "oneshot";
         RemainAfterExit = true;
       };
       script = ''
         install -d -m 0700 "$(${pkgs.coreutils}/bin/dirname '${cfg.luksKeyFile}')"
-        '${cfg.luksKeyGetBin}' '${cfg.luksKeySalt}' > '${cfg.luksKeyFile}'
+        '${luksKeyGetExe}' '${cfg.luksKeySalt}' > '${cfg.luksKeyFile}'
         chmod 600 '${cfg.luksKeyFile}'
       '';
     };
