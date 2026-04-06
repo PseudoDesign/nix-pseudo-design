d175e42463e49313d8d6d0175f935c11dcf263c2
Adam Schafer <ams-tech@users.noreply.github.com>
2026-04-05 20:45:30 -0400

    Add OpenVPN wrong-CA check

--- checks/default.nix
+++ checks/default.nix
@@ -1,6 +1,7 @@
 { pkgs, ... }:
 {
   openvpn-smoke = pkgs.callPackage ./openvpn-smoke.nix { };
+  openvpn-wrong-ca = pkgs.callPackage ./openvpn-wrong-ca.nix { };
   pd-nix-installer = pkgs.callPackage ./pd-nix-installer.nix { };
   rpi-otp-luks-key = pkgs.callPackage ./rpi-otp-luks-key.nix { };
 }
