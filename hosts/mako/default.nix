0ffd9d0e141eacec24dc1db44ef8f71bbf1ec220
Adam Schafer <ams-tech@users.noreply.github.com>
2026-05-09 14:11:06 -0400

    add simple landing page for pseudo.design

--- hosts/mako/default.nix
+++ hosts/mako/default.nix
@@ -1,3 +1,37 @@
 {
-  networking.hostName = "mako";
+  networking = {
+    hostName = "mako";
+    firewall.allowedTCPPorts = [
+      80
+      443
+    ];
+  };
+
+  security.acme = {
+    acceptTerms = true;
+    defaults.email = "admin@pseudo.design";
+  };
+
+  services.nginx = {
+    enable = true;
+    recommendedGzipSettings = true;
+    recommendedOptimisation = true;
+    recommendedTlsSettings = true;
+
+    virtualHosts."pseudo.design" = {
+      root = ./site;
+      enableACME = true;
+      forceSSL = true;
+
+      locations."/".tryFiles = "$uri $uri/ =404";
+
+      extraConfig = ''
+        add_header Strict-Transport-Security "max-age=31536000" always;
+        add_header Content-Security-Policy "default-src 'self'; base-uri 'none'; frame-ancestors 'none'; form-action 'self'; style-src 'self'; img-src 'self'; object-src 'none'; script-src 'none'; upgrade-insecure-requests" always;
+        add_header X-Content-Type-Options "nosniff" always;
+        add_header Referrer-Policy "strict-origin-when-cross-origin" always;
+        add_header X-Frame-Options "DENY" always;
+      '';
+    };
+  };
 }
